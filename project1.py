1a7752517ccd1ced5c9420d4402edd5e4009cc58
GaganNair <gagannair122@gmail.com>
2024-06-03 18:49:27 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,8 @@
+colour=input("enter from the colour red,yellow,green")
+colour=colour.lower()
+if colour=="red":
+    print("sorry you cannot go")
+elif colour=="yellow":
+    print("sorry you have to wait")
+else:
+    print("you can go")
